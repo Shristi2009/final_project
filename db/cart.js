b9a8d259c502364152a323f80973950545e10f3f
Shristi Maharjan <maharjanshristi60@yahoo.com>
2021-10-09 09:37:01 -0500

    cart.js added with functions

---
+++ db/cart.js
@@ -0,0 +1,97 @@
+const client= require('./client');
+
+
+async function createCart({ 
+    usersId, 
+    itemsId,
+    processed,
+    inProcess
+    
+  }) {
+    try {
+      const { rows: [ cart ] } = await client.query(`
+        INSERT INTO cart("usersId", "itemsId", processed, "inProcess") 
+        VALUES($1, $2, $3, $4)
+        ON CONFLICT ("usersId", "itemsId") DO NOTHING 
+        RETURNING *;
+      `, [usersId, itemsId, processed, inProcess]);
+  
+      return cart;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async function getCartById(id) {
+    try {
+      const { rows: [ cart ] } = await client.query(`
+        SELECT *
+        FROM cart
+        WHERE id=${ id }
+      `);
+      //console.log("GET ROUTINE BY ID" ,cart); 
+      return cart;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async function getCartByUsersId(usersId) {
+    try {
+      const { rows: [ cart] } = await client.query(`
+        SELECT *
+        FROM cart
+        WHERE id=${ usersId }
+      `);
+    
+  
+      
+      return cart;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+
+
+  
+  
+  async function deleteCart (id) {
+    try {
+        
+       await client.query(`
+        DELETE 
+        FROM cart
+        WHERE id=${id}
+      `);
+    
+    
+     
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async function removeItem (itemsId) {
+    try {
+        
+       await client.query(`
+        DELETE 
+        FROM cart
+        WHERE "itemsId"=${itemsId}
+      `);
+    
+    
+     
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  module.exports = {
+    getCartById,
+    createCart,
+    removeItem,
+    deleteCart,
+    getCartByUsersId
+}
