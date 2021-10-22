837a9e5a0305ccfda0a5c1a98c8e2ce8d3fe1eeb
Shristi Maharjan <maharjanshristi60@yahoo.com>
2021-10-22 09:58:29 -0500

    debugged cart

--- db/cart.js
+++ db/cart.js
@@ -98,13 +98,14 @@ async function createCart({
     try {
       
   
-      const {rows:[items]} = await client.query(`
+      const {rows} = await client.query(`
         SELECT * FROM items
         JOIN cart ON "itemsId" = items.id
         WHERE "usersId" = ${usersId};
+        
       `);
-      
-      return items;
+      console.log("ITEMS", rows)
+      return rows;
     } catch (error) {
       throw error;
     }
